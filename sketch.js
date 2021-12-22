36f880afc6812dffccc8b381b854259b12bdf1fd
Asmi-dj <96016808+Asmi-dj@users.noreply.github.com>
2021-12-22 09:49:17 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var weight = [35,38,42,45,43,34,36,41,48,32];
+
+function cal_avg () {
+
+var sum = 0; 
+for (var i = 0; i < weight.length; i++) {
+
+  sum = sum+ weight[i];
+
+  var avg = sum/weight.length;
+
+  
+}
+console.log ("The average is : " + avg);
+
+
+}
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+       cal_avg();
+}
+
+function draw() 
+{
+  background(30);
+}
+
+ 
+
